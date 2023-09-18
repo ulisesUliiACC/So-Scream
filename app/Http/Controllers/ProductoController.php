b1ff4199127bfc8c6fc0ae5a0ab2adac650e97cb
ulises Uliii <ulisesuliiuwu@gmail.com>
2023-09-17 20:48:39 -0600

    logica de funcion store de productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class ProductoController extends Controller
@@ -34,7 +36,50 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'nombre_producto' => 'required|string|max:255|unique:productos',
+            'descripcion' => 'required|string',
+            'precio' => 'required|numeric',
+            'stock' => 'required|integer',
+            'status' => 'required|string',
+            'fecha_activo' => 'required|date',
+            'fecha_inactivo' => 'required|date|after:fecha_activo',
+            'imagen' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+        
+            // Procesar la imagen   
+        if ($validator->fails()) {
+            return redirect()
+                ->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        if ($request->hasFile('imagen')) {
+            $imagen = $request->file('imagen');
+            $imagenNombre = time() . '_' . $imagen->getClientOriginalName();
+            $imagen->storeAs('public/imagenes', $imagenNombre);
+    
+            // Guardar la ruta de la imagen en la base de datos
+            $Producto = new Producto();
+            $Producto->imagen = 'storage/imagenes/' . $imagenNombre;
+        } else {
+            $Producto = new Producto();
+        }
+
+        $Producto->nombre_producto = $request -> input('nombre_producto');
+        $Producto->descripcion = $request-> input('descripcion');
+        $Producto->precio = $request -> input('precio');
+        $Producto->stock = $request -> input('stock');
+        $Producto->status=$request->input('status');
+        $Producto->fecha_activo=$request->input('fecha_activo');
+        $Producto->fecha_inactivo=$request->input('fecha_inactivo');
+        $Producto->imagen=$request->input('imagen');
+        $Producto ->save();
+
+        return redirect()->route('productos.index');
+
+        
     }
 
     /**
