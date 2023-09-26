d2c8c1dde00b972c9a8fd16093021db3b8148ddf
ulises Uliii <ulisesuliiuwu@gmail.com>
2023-09-26 02:49:11 -0600

    login de usuarios de clientes y login de administradores

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts/contentNavbarLayout')
+
+@section('title', 'ulises')
+
+@section('vendor-style')
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/apex-charts/apex-charts.css') }}">
+@endsection
+
+@section('vendor-script')
+    <script src="{{ asset('assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+@endsection
+
+@section('page-script')
+    <script src="{{ asset('assets/js/dashboards-analytics.js') }}"></script>
+@endsection
+
+@section('content')
+  
+@endsection
